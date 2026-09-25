756d87f4f823700faee9267d443aacdf256e1734
sbrafiqahmedali7575-ai <sbrafiqahmedali7575@gmail.com>
2026-09-25 12:06:56 +0530

    Add unique icons for data analyst badges

---
+++ src/components/BadgeIcon.tsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import {
+  Search,
+  Database,
+  Table2,
+  Code2,
+  Eraser,
+  Wrench,
+  BarChart3,
+  TerminalSquare,
+  LayoutDashboard,
+  Monitor,
+  Sigma,
+  Network,
+  FileText,
+  Lightbulb,
+  Briefcase,
+  Target,
+  Users,
+  ShoppingCart,
+  Settings,
+  WalletCards,
+  Package,
+  Megaphone,
+  Cpu,
+  PanelsTopLeft,
+  Award,
+  Flag,
+  Crown,
+} from 'lucide-react';
+import { LongTermBadge } from '../utils/badgeSystem';
+
+interface BadgeIconProps {
+  badge: LongTermBadge;
+  className?: string;
+}
+
+const badgeIcons: Record<string, React.ComponentType<{ className?: string }>> = {
+  trainee: Search,
+  'first-scroll': Database,
+  'day-15': Table2,
+  'shuriken-bronze': Code2,
+  'day-45': Eraser,
+  'day-60': Wrench,
+  'day-75': BarChart3,
+  'silent-scout': TerminalSquare,
+  'crimson-guardian': LayoutDashboard,
+  'day-270': Monitor,
+  'day-360': Sigma,
+  'day-450': Network,
+  'day-540': FileText,
+  'day-630': Lightbulb,
+  'day-720': Briefcase,
+  'day-810': Target,
+  'day-900': Users,
+  'day-990': ShoppingCart,
+  'day-1080': Settings,
+  'day-1170': WalletCards,
+  'day-1260': Package,
+  'day-1350': Megaphone,
+  'day-1440': Cpu,
+  'day-1530': PanelsTopLeft,
+  'day-1620': Award,
+  'day-1710': Flag,
+  'day-1800': Crown,
+};
+
+export const BadgeIcon: React.FC<BadgeIconProps> = ({ badge, className = 'w-4 h-4' }) => {
+  const Icon = badgeIcons[badge.id] ?? Award;
+  return <Icon className={className} />;
+};
